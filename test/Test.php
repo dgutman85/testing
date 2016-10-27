2833365f771a5eecd430d3d0df32b91358aa0634
Diego Gutman <diego.gutman85@gmail.com>
2016-10-27 09:29:53 -0300

    Primer Commit

---
+++ test/Test.php
@@ -0,0 +1,14 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class Test extends TestCase
+{
+    /**
+    * @test
+    */
+    public function testFirst()
+    {
+        $this->assertTrue(true);
+    }
+}
